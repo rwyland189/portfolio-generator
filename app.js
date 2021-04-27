89051dbde21245a02a55fa58c5b2cbd3d3a82552
Becca Wyland <rwyland@uwalumni.com>
2021-04-26 22:33:51 -0500

    question and answer objects, add multiple projects

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const inquirer = require("inquirer");
+const { prototype } = require("inquirer/lib/objects/choice");
 // //The require statement is necessary to use the file system (fs) module
 // const fs = require("fs");
 
@@ -17,13 +18,90 @@ const inquirer = require("inquirer");
 //     console.log("Portfolio complete! Check out index.html to see the output!");
 // });
 
-inquirer
-    .prompt([
+const promptUser = () => {
+    return inquirer.prompt([
         // Question object
         {
             type: "input",
             name: "name",
             message: "What is your name?"
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "Enter your GitHub Username"
+        },
+        {
+            type: "input",
+            name: "about",
+            message: "Provide some information about yourself:"
         }
+    ]);
+};
+
+const promptProject = portfolioData => {
+    //store project data, if loop to only initialize on first pass
+    if (!portfolioData.projects) {
+        portfolioData.projects = [];
+    }
+ 
+    console.log(`
+=================
+Add a New Project
+=================
+`);
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is the name of your project?"
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Provide a description of the project. (Required)"
+        },
+        {
+            type: "checkbox",
+            name: "languages",
+            message: "What did you build this project with? (Check all that apply)",
+            choices: ["Javascript", "HTML", "CSS", "ES6", "jQuery", "Bootstrap", "Node"]
+        },
+        {
+            type: "input",
+            name: "link",
+            message: "Enter the GitHub link to your project. (Required)"
+        },
+        {
+            type: "confirm",
+            name: "feature",
+            message: "Would you like to feature this project?",
+            default: false
+        },
+        {
+            type: "confirm",
+            name: "confirmAddProject",
+            message: "Would you  like to enter another project?",
+            default: false
+        },
+        
     ])
-    .then(answers => console.log(answers));
+    .then(projectData => {
+        portfolioData.projects.push(projectData);
+        // Where did projectData come from?
+        // If user confirms they would like to add another project, this condition will evaluate to true and call the promptProject(portfolioData) function
+        if (projectData.confirmAddProject) {
+            // portfolioData must be included as the argument when calling this promptProject function otherwise a new projects array will be initialized and the existing project data will be lost
+            return promptProject(portfolioData);
+            // If user does not want to add another project, the condition will evaluate to false. portfolioData is returned in the else statement explicitly so that the object is returned --> critical step to retrieving user's answre and building HTML template
+        } else {
+            return portfolioData;
+        }
+    });
+};
+
+promptUser()
+    .then(promptProject)
+    .then(portfolioData => {
+        console.log(portfolioData);
+    });
